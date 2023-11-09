1b9e38539de6cbd04dbc0e66b196add2ff066131
David Mellen <d.mellen@remind.de>
2023-11-09 10:06:18 +0100

    Update config and dependencies and use npm instead of yarn

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 module.exports = {
     root: true,
-    env: {
-        node: true,
-    },
     extends: [
-        'plugin:@typescript-eslint/recommended',
-        '@nuxtjs/eslint-config-typescript',
-        'plugin:prettier/recommended',
-        'plugin:nuxt/recommended',
+        "@nuxt/eslint-config",
+        "plugin:prettier/recommended",
     ],
-    parserOptions: {
-        ecmaVersion: 2020,
-    },
     rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-inferrable-types': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
-        'prettier/prettier': [
-            'error',
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+                argsIgnorePattern: "^_"
+            }
+        ],
+        "prettier/prettier": [
+            "error",
             {
                 singleQuote: true,
                 semi: false,
             },
         ],
-        'import/namespace': [
-            2,
-            {
-                allowComputed: true,
-            },
-        ],
     },
-}
+};
