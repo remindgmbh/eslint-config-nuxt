5f68abef60677a5acf96e08470f8f7de50312fb7
David Mellen <d.mellen@remind.de>
2023-11-30 13:37:02 +0100

    Add no-console rule

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
         "plugin:prettier/recommended",
     ],
     rules: {
+        "no-console": "warn",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-inferrable-types": "off",
         "@typescript-eslint/no-empty-function": "off",
