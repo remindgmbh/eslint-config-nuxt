1d9ea5d8da3ec97230c0d1407d93cd3e52754af0
David Mellen <d.mellen@remind.de>
2024-05-16 12:42:01 +0200

    fix: add missing import in types

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ import type {
     FlatConfigComposer,
     ResolvableFlatConfig,
 } from 'eslint-flat-config-utils'
+import type { Linter } from 'eslint'
 import type { NuxtESLintConfigOptions } from '@nuxt/eslint-config/flat'
 
 declare function createConfigForNuxt(
